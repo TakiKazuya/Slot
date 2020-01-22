1ebafa1be5a4276850f942d2c5af54471ee64940
kazup1125 <kazup1125@me.com>
2020-01-22 16:12:26 +0900

    TimerModel作成（仮）

--- Slot/TimerModel.swift
+++ Slot/TimerModel.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import UIKit
+
+class TimerModel{
+    
+    var speed = Int()
+    var imageView = UIImageView()
+    var imageArray = [UIImage]()
+    var count = Int()
+    var timer:Timer!
+    var target:Any
+    
+    init(speed:Int,imageView:UIImageView,imageArray:[UIImage],count:Int,target:Any) {
+        self.speed = speed
+        self.imageView = imageView
+        self.imageArray = imageArray
+        self.count = count
+        self.target = target
+    }
+    
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(speed), target: target, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerUpdate() {
+        count += 1
+        if count >= 9{
+            count = 0
+        }
+        imageView.image = imageArray[count]
+    }
+}
